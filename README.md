5c9bf9845c0be8ebc3d8a8c8aac35ff0410abadc
Douglas Hartmann <douglas.dhl@gmail.com>
2020-01-27 21:07:19 -0300

    Ajustes

--- README.md
+++ README.md
@@ -10,3 +10,7 @@ Ex:
 //route params = /users/1
 
 //request body = { "nome" : "Douglas", "data_nas" : "25/01/1992" } 
+
+#Após criar os metodos de CRUD, você pode criar os middlewares. 
+#No qual básicamente interceptam o que é enviado nos metodos do CRUD que são as váriaveis de paramentro req e res
+#middlewares podem alterar as váriaveis req e ras, mais a váriavel req, que você pode setar um valor para este array
--- index.js
+++ index.js
@@ -2,16 +2,86 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+
 //query params = ?teste=1
 //route params = /users/1
 //request body = { "nome" : "Douglas", "data_nas" : "25/01/1992" } 
 
-server.get('/users/:id', (req, res) => {
+const arrayUsuarios = ['Douglas', 'Manu', 'Jacqueline', 'Jhou'];
+
+server.use((req, res, next) =>{
+    console.time('Request');
+    console.log(`Método: ${req.method} URL: ${req.url}`);
+
+    next();
+
+    console.timeEnd('Request');
+});
+
+function usuarioExiste(req, res, next){
+    if(!req.body.nome){
+        return res.status(400).json({ "error" : "Nome do usuário é necessário" });
+    }
+
+    return next();
+}
+
+function verificarUsuarioArray(req, res, next){
+
+    const user = arrayUsuarios[req.params.index];
+
+    if(!user){
+        return res.status(400).json({ "error" : "Usuário inexistente" });
+    }
+
+    req.user = user;
+
+    return next();
+}
+
+server.post('/users', usuarioExiste, (req, res) => {
+    const { nome } = req.body;
+
+    arrayUsuarios.push(nome);
+
+    return res.json(arrayUsuarios);
+});
+
+server.put('/users/:index', usuarioExiste, verificarUsuarioArray, (req, res) => {
+    const { nome } = req.body;
+    const { index } = req.params;
+    
+    arrayUsuarios[index] = nome;
+
+    return res.json(arrayUsuarios);
+});
+
+server.delete('/users/:index', verificarUsuarioArray, (req, res) => {
+    const { index } = req.params;
+    
+    arrayUsuarios.splice(index, 1)
+
+    return res.json(arrayUsuarios);
+});
+
+server.get('/users', (req, res) => {
+    return res.json(arrayUsuarios);
+});
+
+server.get('/usersArr/:index', verificarUsuarioArray, (req, res) => {
+
+    const { index } = req.params;
+
+    return res.json({ message: `Olá ${req.user}` });
+});
+
+server.get('/user/:id', (req, res) => {
 
     const nome = req.query.nome;
     const id = req.params.id;
 
-    return res.json({ message: `Olá id: ${id} do nome ${nome}` });
+    return res.json({ message: `Diga: Olá id: ${id} do nome ${nome}` });
 });
 
 
